fd9c6d60f397a9389f4a2d22da4c4be242369433
Rasim <rasimviolet@mail.ru>
2017-02-21 19:33:31 +0300

    user input

---
+++ calculator.java
@@ -0,0 +1,16 @@
+public class calculator {
+	private int result;
+	
+	public void add(int ... params){
+	     for (Integer param: params)	
+		this.result += param;
+	}
+	
+	public int getResult(){
+		return this.result;
+	}
+	
+	public void clearResult(){
+		this.result = 0;
+	}
+}
